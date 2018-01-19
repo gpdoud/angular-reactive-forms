68d0de269db01321d297ee983d305aaa79705207
Greg doud <gpdoud@gmail.com>
2018-01-19 13:45:03 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+  template: `    
+    <form [formGroup]="form" (ngSubmit)="onSubmit()">
+        Name: <input class="form-control" type="text" formControlName="name" />
+      <div>
+        <span [hidden]="form.controls.name.valid">Name is required</span>
+      </div>
+      <div>
+        Role: <input class="form-control" type="text" formControlName="role" />
+        <span [hidden]="form.controls.role.valid">Role is required</span>
+      </div>
+      <div>
+        Age: <input class="form-control" type="number" formControlName="age" />
+        <span [hidden]="form.controls.age.valid">Age is required</span>
+      </div>
+      <div>
+        Age: <input class="form-control" type="date" formControlName="bday" />
+        <span [hidden]="form.controls.bday.valid">Birthday is required</span>
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit">Submit</button>
+      </div>
+    </form>
   `,
-  styles: []
+  styles: [
+    "input.ng-invalid { border-left: 5px solid #a94442; }",
+    "input.ng-valid { border-left: 5px solid #42a948; }",
+    "form { padding: 20px; }"
+  ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  form: FormGroup;
+  model = { id: 1, name: 'Greg', role: 'instructor', age: 60, bday: '1957-08-27' };
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    console.log("onSubmit()", this.model);
+  }
+
+  ngOnInit() {
+
+    this.form = this.formBuilder.group({
+      name: [this.model.name, Validators.required],
+      role: [this.model.role, Validators.required],
+      age: [this.model.age, Validators.required],
+      bday: [this.model.bday, Validators.required]
+    });
+
+  }
 }
